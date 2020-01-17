41a3220074b7c26a1081cb9d3e4d9328309dfafc
Alejandro cortez <alejandro.cortes@ironbit.com.mx>
2020-01-17 15:16:28 -0600

    Added custom types

--- index.js
+++ index.js
@@ -1,41 +1,28 @@
 'use strict';
 
-const { graphql, buildSchema } = require('graphql');
+const { buildSchema } = require('graphql');
 const express = require('express');
 const gqlMiddleware = require('express-graphql');
+const { readFileSync } = require('fs');
+const { join } =  require('path');
+const resolvers = require('./lib/resolvers');
 
 const app = express();
 const port = process.env.port || 3000;
 // Initial Schema
 
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-        saludo: String
-    }
-`);
-
-// Resolver
-
-const resolver =  {
-    hello: () => 'Hello world',
-    saludo: () => 'Qiubolas perro'
-};
-
-// Exec schema
-
-/*
-graphql(schema, '{ saludo }', resolver). then((data) => {
-    console.log(data);
-});
-*/
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    ));
 
 app.use('/api', gqlMiddleware({
-    schema,
-    rootValue: resolver,
-    graphiql: true
+  schema,
+  rootValue: resolvers,
+  graphiql: true
 }));
 
-app.listen(port,() => {
-    console.log(`Server is listening at http://localhost:${port}/api`);
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
 });
