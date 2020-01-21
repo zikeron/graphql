d65f1b4c73785535afb38d34d093321e4a2ce75c
Alejandro cortez <alejandro.cortes@ironbit.com.mx>
2020-01-21 15:52:16 -0600

    Added mongodb connection

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 'use strict';
-
-const { buildSchema } = require('graphql');
+require('dotenv').config();
+const {makeExecutableSchema} = require('graphql-tools');
 const express = require('express');
 const gqlMiddleware = require('express-graphql');
-const { readFileSync } = require('fs');
-const { join } =  require('path');
+const {readFileSync} = require('fs');
+const {join} = require('path');
 const resolvers = require('./lib/resolvers');
 
 const app = express();
 const port = process.env.port || 3000;
 // Initial Schema
-
-const schema = buildSchema(
-    readFileSync(
-        join(__dirname, 'lib', 'schema.graphql'),
-        'utf-8'
-    ));
+const typeDefs = readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+);
+const schema = makeExecutableSchema({typeDefs, resolvers});
 
 app.use('/api', gqlMiddleware({
-  schema,
-  rootValue: resolvers,
-  graphiql: true
+    schema,
+    rootValue: resolvers,
+    graphiql: true
 }));
 
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}/api`)
+    console.log(`Server is listening at http://localhost:${port}/api`)
 });
